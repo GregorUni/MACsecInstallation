1621db43816a0a09bcdd8d5e3507e7e650e254b2
GregorUni <gregor.garten@mailbox.tu-dresden.de>
2020-08-27 14:24:11 +0200

    pushing updates

--- Installation/MACsec/MACsec5.4/.macsec.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/pi/MACsecInstallation/Installation/MACsec/MACsec5.4/macsec.ko := ld -r  -EL  --build-id  -T ./scripts/module-common.lds -T ./arch/arm/kernel/module.lds -o /home/pi/MACsecInstallation/Installation/MACsec/MACsec5.4/macsec.ko /home/pi/MACsecInstallation/Installation/MACsec/MACsec5.4/macsec.o /home/pi/MACsecInstallation/Installation/MACsec/MACsec5.4/macsec.mod.o;  true
