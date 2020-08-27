8496f8808d55660a95570934aa1517dff1c54cea
GregorUni <gregor.garten@mailbox.tu-dresden.de>
2020-08-27 15:36:46 +0200

    update

--- Installation/PrototypeMACsec/macsec.c
+++ Installation/PrototypeMACsec/macsec.c
@@ -362,8 +362,8 @@ static void free_rxsa(struct rcu_head *head)
 {
 	struct macsec_rx_sa *sa = container_of(head, struct macsec_rx_sa, rcu);
 
-	crypto_free_aead(sa->key->tfm[0]);
-	crypto_free_aead(sa->key->tfm[1]);
+	crypto_free_aead(sa->key[0].tfm);
+	crypto_free_aead(sa->key[1].tfm);
 	free_percpu(sa->stats);
 	kfree(sa);
 }
@@ -391,8 +391,8 @@ static void free_txsa(struct rcu_head *head)
 {
 	struct macsec_tx_sa *sa = container_of(head, struct macsec_tx_sa, rcu);
 
-	crypto_free_aead(sa->key->tfm[0]);
-	crypto_free_aead(sa->key->tfm[1]);
+	crypto_free_aead(&sa->key[0].tfm);
+	crypto_free_aead(&sa->key[1].tfm);
 	
 	free_percpu(sa->stats);
 	kfree(sa);
